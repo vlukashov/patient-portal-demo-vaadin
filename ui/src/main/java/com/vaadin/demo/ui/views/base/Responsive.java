32c5505cc9dda12e3df1e7b385603d9f192c9a84
Marcus Hellberg <marcus@vaadin.com>
2017-03-27 15:07:49 -0700

    Make patients grid responsive...ish

---
+++ ui/src/main/java/com/vaadin/demo/ui/views/base/Responsive.java
@@ -0,0 +1,29 @@
+package com.vaadin.demo.ui.views.base;
+
+import com.vaadin.server.ClientConnector;
+import com.vaadin.server.Page;
+import com.vaadin.shared.Registration;
+
+public interface Responsive {
+
+    default ClientConnector.DetachListener addResponsiveListener(WindowResizeListener listener) {
+
+        Registration registration = Page.getCurrent().addBrowserWindowResizeListener(dimensions ->
+                listener.sizeUpdated(getLayoutMode()));
+
+        // Kickstart with current width
+        listener.sizeUpdated(getLayoutMode());
+
+        return (ClientConnector.DetachListener) event -> registration.remove();
+    }
+
+    default Mode getLayoutMode() {
+        return Page.getCurrent().getBrowserWindowWidth() < 900 ? Mode.NARROW : Mode.WIDE;
+    }
+
+    interface WindowResizeListener {
+        void sizeUpdated(Mode mode);
+    }
+
+    enum Mode {WIDE, NARROW}
+}
--- ui/src/main/java/com/vaadin/demo/ui/views/patients/PatientsView.java
+++ ui/src/main/java/com/vaadin/demo/ui/views/patients/PatientsView.java
@@ -1,26 +1,34 @@
 package com.vaadin.demo.ui.views.patients;
 
-import com.vaadin.annotations.Viewport;
 import com.vaadin.demo.entities.Patient;
 import com.vaadin.demo.ui.views.base.CssLayoutView;
+import com.vaadin.demo.ui.views.base.Responsive;
+import com.vaadin.icons.VaadinIcons;
 import com.vaadin.navigator.View;
 import com.vaadin.navigator.ViewChangeListener;
 import com.vaadin.server.Page;
+import com.vaadin.shared.Registration;
 import com.vaadin.spring.annotation.SpringComponent;
 import com.vaadin.spring.annotation.SpringView;
+import com.vaadin.ui.FormLayout;
 import com.vaadin.ui.Grid;
+import com.vaadin.ui.Label;
+import com.vaadin.ui.NativeButton;
+import com.vaadin.ui.renderers.ComponentRenderer;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import javax.annotation.PostConstruct;
 import java.text.SimpleDateFormat;
+import java.util.Optional;
 
 @SpringComponent
 @SpringView(name = "patients")
-public class PatientsView extends CssLayoutView implements View {
+public class PatientsView extends CssLayoutView implements View, Responsive {
 
     private final PatientDetailsView detailsView;
     private final PatientsService patientsService;
     private Grid<Patient> patientsGrid;
+    private Registration gridSelectionRegistration;
 
     @Autowired
     public PatientsView(PatientDetailsView detailsView, PatientsService patientsService) {
@@ -32,8 +40,10 @@ public class PatientsView extends CssLayoutView implements View {
     @PostConstruct
     void init() {
         setSizeFull();
-        buildLayout();
         addComponent(detailsView);
+        patientsGrid = new Grid<>();
+        patientsGrid.setSizeFull();
+        addComponent(patientsGrid);
     }
 
     @Override
@@ -44,17 +54,71 @@ public class PatientsView extends CssLayoutView implements View {
     @Override
     public void attach() {
         super.attach();
+        setupGrid();
+
         addSubscription(patientsService.getPatients().subscribe(p -> {
             patientsGrid.setItems(p);
-            patientsService.getCurrentPatient().getValue().ifPresent(selected -> patientsGrid.select(selected));
+            if (getLayoutMode() == Mode.WIDE) {
+                patientsService.getCurrentPatient().getValue().ifPresent(selected -> patientsGrid.select(selected));
+            }
         }));
-        addSubscription(patientsService.getCurrentPatient().distinct().subscribe(p -> p.ifPresent(patient -> patientsGrid.select(patient))));
+        addSubscription(patientsService.getCurrentPatient().distinct().subscribe(p -> {
+            if (getLayoutMode() == Mode.WIDE) {
+                p.ifPresent(patient -> patientsGrid.select(patient));
+            }
+        }));
     }
 
-    private void buildLayout() {
-        patientsGrid = new Grid<>();
-        patientsGrid.setSizeFull();
+
+    private void setupGrid() {
+        addDetachListener(addResponsiveListener(mode -> {
+            if (mode == Mode.NARROW) {
+                setupNarrowGrid();
+            } else if (mode == Mode.WIDE) {
+                setupWideGrid();
+            }
+        }));
+    }
+
+    private void setupNarrowGrid() {
+        if (gridSelectionRegistration != null) {
+            gridSelectionRegistration.remove();
+            gridSelectionRegistration = null;
+        }
+        patientsGrid.setSelectionMode(Grid.SelectionMode.NONE);
+        patientsGrid.removeAllColumns();
+
+        patientsGrid.addColumn(patient -> {
+            NativeButton toggleButton = new NativeButton();
+            toggleButton.setIcon(VaadinIcons.CHEVRON_RIGHT);
+            toggleButton.addClickListener(click -> {
+                boolean visible = !patientsGrid.isDetailsVisible(patient);
+                patientsGrid.setDetailsVisible(patient, visible);
+                // Changing the icon doesn't get updated on the client for some reason
+                toggleButton.setIcon(visible ? VaadinIcons.CHEVRON_DOWN : VaadinIcons.CHEVRON_RIGHT);
+            });
+            return toggleButton;
+        }, new ComponentRenderer());
+        patientsGrid.addColumn(patient -> patient.getLastName() + ", " + patient.getFirstName()).setId("lastName").setCaption("Name").setExpandRatio(1);
+        patientsGrid.addColumn(patient -> {
+            NativeButton detailsButton = new NativeButton();
+            detailsButton.setIcon(VaadinIcons.ARROW_CIRCLE_RIGHT);
+            detailsButton.addClickListener(click -> patientsService.getCurrentPatient().onNext(Optional.of(patient)));
+            return detailsButton;
+        }, new ComponentRenderer());
+        patientsGrid.setDetailsGenerator(PatientDetailsRow::new);
+        patientsGrid.getDataProvider().refreshAll();
+    }
+
+
+    private void setupWideGrid() {
+        if (gridSelectionRegistration != null) {
+            gridSelectionRegistration.remove();
+            gridSelectionRegistration = null;
+        }
         patientsGrid.setSelectionMode(Grid.SelectionMode.SINGLE);
+        patientsGrid.removeAllColumns();
+        patientsGrid.setDetailsGenerator(null);
 
         patientsGrid.addColumn(patient -> patient.getLastName() + ", " + patient.getFirstName()).setId("lastName").setCaption("Name");
         patientsGrid.addColumn(patient -> patient.getId().toString()).setId("id").setCaption("Id");
@@ -65,7 +129,27 @@ public class PatientsView extends CssLayoutView implements View {
         ).setId("lastVisit").setCaption("Last visit");
 
 
-        patientsGrid.addSelectionListener(e -> patientsService.getCurrentPatient().onNext(e.getFirstSelectedItem()));
-        addComponent(patientsGrid);
+        gridSelectionRegistration = patientsGrid.addSelectionListener(e -> patientsService.getCurrentPatient().onNext(e.getFirstSelectedItem()));
+        patientsGrid.getDataProvider().refreshAll();
+    }
+
+
+    class PatientDetailsRow extends FormLayout {
+        PatientDetailsRow(Patient patient) {
+            addStyleName("patient-details-row");
+            setMargin(true);
+            setWidth("100%");
+
+            Label id = new Label(patient.getId().toString());
+            id.setCaption("Id");
+            Label medicalRecord = new Label(patient.getMedicalRecord().toString());
+            medicalRecord.setCaption("Medical Record");
+            Label doctor = new Label(patient.getDoctor().getLastName() + ", " + patient.getDoctor().getFirstName());
+            doctor.setCaption("Doctor");
+            Label lastVisit = new Label(patient.getLastVisit() == null ? "" : SimpleDateFormat.getDateInstance().format(patient.getLastVisit()));
+            lastVisit.setCaption("Last Visit");
+
+            addComponents(id, medicalRecord, doctor, lastVisit);
+        }
     }
 }

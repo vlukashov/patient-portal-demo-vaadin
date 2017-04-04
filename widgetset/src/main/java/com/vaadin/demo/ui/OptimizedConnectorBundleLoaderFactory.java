b030549ce299352f12031fed7b7c526eace87b1c
Marcus Hellberg <marcus@vaadin.com>
2017-04-04 13:57:21 -0700

    trying to fix widgetset optimization

--- widgetset/src/main/java/com/vaadin/demo/ui/OptimizedConnectorBundleLoaderFactory.java
+++ widgetset/src/main/java/com/vaadin/demo/ui/OptimizedConnectorBundleLoaderFactory.java
@@ -14,17 +14,18 @@ public class OptimizedConnectorBundleLoaderFactory extends
 
     {
         eagerConnectors.add(com.vaadin.client.ui.ui.UIConnector.class.getName());
-        eagerConnectors.add(com.vaadin.client.ui.formlayout.FormLayoutConnector.class.getName());
+        eagerConnectors.add(com.vaadin.client.ui.nativebutton.NativeButtonConnector.class.getName());
+        eagerConnectors.add(com.vaadin.client.ui.passwordfield.PasswordFieldConnector.class.getName());
         eagerConnectors.add(com.vaadin.client.ui.textfield.TextFieldConnector.class.getName());
         eagerConnectors.add(com.vaadin.client.ui.label.LabelConnector.class.getName());
-        eagerConnectors.add(com.vaadin.client.ui.passwordfield.PasswordFieldConnector.class.getName());
-        eagerConnectors.add(com.vaadin.client.ui.nativebutton.NativeButtonConnector.class.getName());
         eagerConnectors.add(com.vaadin.client.ui.orderedlayout.VerticalLayoutConnector.class.getName());
+        eagerConnectors.add(com.vaadin.client.ui.button.ButtonConnector.class.getName());
     }
 
     private Set<String> deferredConnectors = new HashSet<String>();
     {
         deferredConnectors.add(com.vaadin.client.connectors.grid.DetailsManagerConnector.class.getName());
+        deferredConnectors.add(com.vaadin.client.ui.formlayout.FormLayoutConnector.class.getName());
         deferredConnectors.add(com.vaadin.client.connectors.grid.TextRendererConnector.class.getName());
         deferredConnectors.add(com.vaadin.client.connectors.grid.ColumnConnector.class.getName());
         deferredConnectors.add(com.vaadin.client.connectors.grid.GridConnector.class.getName());
